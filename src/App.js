ceb393f2ecf5edeccdb623890dee67d824568168
yashcpanchal <yashcpanchal41@gmail.com>
2025-05-16 15:15:32 -0400

    Made the navbar and navbar css. Started working on the about me.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
+import Navbar from './components/Navbar.js';
+import About from './components/About.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <About />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+function About() {
+    return (
+        <section className="about">
+            <h2>About Me</h2>
+            <p> Hey! I'm Yash, computer science major passionate 
+                about learning new technologes</p>
+            <img src="./yash-picture.png" alt="Photo of Yash Panchal"></img>
+            <ul>
+                <li>Element 1</li>
+                <li>Element 2</li>
+            </ul>
+        </section>
+    )
+}
+
+export default About;
