dded61f210215c92099cad65fbe1d61fce3d7a45
yashcpanchal <yashcpanchal41@gmail.com>
2025-05-22 19:24:07 -0400

    added the typewriter text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,33 @@
 import './Home.css';
 import { FaGithub } from 'react-icons/fa';
-import ParticlesBackground from '../ParticlesBackground';
+import ParticlesBackground from './ParticlesBackground';
+import { Typewriter } from 'react-simple-typewriter';
 
 function Home() {
+    function TypewriterText() {
+        return (
+            <h1>
+                I'm a&nbsp;
+                <span className='typewriter'>
+                    <Typewriter 
+                        words={['Developer', 'Designer', 'Problem Solver']}
+                        loop={true}
+                        cursor
+                        cursorStyle="|"
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                    />
+                </span>
+            </h1>
+        )
+    }
     return (
         <div>
             <ParticlesBackground />
             <section id="home" className="home">
                 <h3>Hi, I'm Yash</h3>
+                <TypewriterText />
                 <a href="https://github.com/"><FaGithub /></a>
             </section>
         </div>
